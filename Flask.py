f229c9ba27b2ccb831521c21f344c87c6d060a92
thomas-gresco <tjgresco20@gmail.com>
2023-08-07 18:50:35 -0400

    Updated Charts

--- Flask.py
+++ Flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, send_file
 import sqlite3
 
 app = Flask(__name__)
@@ -29,12 +29,22 @@ def welcome():
         f"- List of all the drivers in Formula 1<br/>"
         f"/api/v1.0/teams<br/>"
         f"- List of all the teams in Formula 1<br/>"
+        f"/api/v1.0/races<br/>"
+        f"- List of all the races in Formula 1<br/>"
         f"/api/v1.0/circuits<br/>"
         f"- List of all the circuits in Formula 1<br/>"
         f"/api/v1.0/driver_standings/<int:race_id><br/>"
         f"- List of the driver standings for a specific race<br/>"
         f"/api/v1.0/team_standings/<int:race_id><br/>"
         f"- List of the team standings for a specific race<br/>"
+        f"/api/v1.0/fastest_lap_times<br/>"
+        f"- List of the fastest lap times for each driver<br/>"
+        f"/api/v1.0/driver_standings_over_time<br/>"
+        f"- List of the driver standings over time for a specific driver<br/>"
+        f"/api/v1.0/map<br/>"
+        f"- Map of all the circuits in Formula 1<br/>"
+        f"/api/v1.0/charts<br/>"
+        f"- Charts of statistics for all the drivers in Formula 1<br/>"
     )
 
 # Route to get all drivers
@@ -54,6 +64,15 @@ def get_teams():
     columns = ('teamId', 'name', 'nationality')
     teams_data = [dict(zip(columns, team)) for team in teams]
     return jsonify(teams_data)
+
+#Route to get all races
+@app.route('/races', methods=['GET'])
+def get_races():
+    query = 'SELECT raceId, year, round, circuitId, name FROM Races'
+    races = execute_query(query)
+    columns = ('raceId', 'year', 'round', 'circuitId', 'name')
+    races_data = [dict(zip(columns, race)) for race in races]
+    return jsonify(races_data)
 
 # Route to get all circuits
 @app.route('/circuits', methods=['GET'])
@@ -84,10 +103,36 @@ def get_team_standings(race_id):
     team_standings_data = [dict(zip(columns, standing)) for standing in team_standings]
     return jsonify(team_standings_data)
 
+# Route to get the top 7 fastest lap times for a specific driver
+@app.route('/fastest_lap_times/<int:driver_id>', methods=['GET'])
+def get_fastest_lap_times_by_driver(driver_id):
+    query = 'SELECT raceId, fastestLapTime FROM results WHERE driverId = ? ' \
+            'ORDER BY fastestLapTime ASC LIMIT 7'
+    params = (driver_id,)
+    lap_times = execute_query(query, params)
+    columns = ('raceId', 'fastestLapTime')
+    lap_times_data = [dict(zip(columns, lap_time)) for lap_time in lap_times]
+    return jsonify(lap_times_data)
+
+# Route to get driver standings over time for a specific driver
+@app.route('/driver_standings_over_time/<int:driver_id>', methods=['GET'])
+def get_driver_standings_over_time(driver_id):
+    query = 'SELECT raceId, points FROM driver_standings WHERE driverId = ? ORDER BY raceId'
+    params = (driver_id,)
+    driver_standings = execute_query(query, params)
+    columns = ('raceId', 'points')
+    driver_standings_data = [dict(zip(columns, standing)) for standing in driver_standings]
+    return jsonify(driver_standings_data)
+
 # Route to serve the map template
 @app.route('/map', methods=['GET'])
 def show_map():
     return render_template('map.html')
+
+#Route to serve the driver chart template
+@app.route('/charts', methods=['GET'])
+def show_charts():
+    return render_template('graphs.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/driver_charts.js
@@ -0,0 +1,214 @@
+// Add the Chart.js library script
+const chartJsScript = document.createElement('script');
+chartJsScript.src = 'https://cdn.jsdelivr.net/npm/chart.js';
+document.head.appendChild(chartJsScript);
+
+let lapTimesChart = null; // Variable to store the Chart instance
+let driverStandingsChart = null; // Variable to store the Chart instance for driver standings
+
+// Function to format time in seconds to "m:ss.SSS" format
+function formatTime(timeInSeconds) {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const secondsWithMilliseconds = (timeInSeconds % 60).toFixed(3).padStart(6, '0'); // Ensure 3 decimal places and pad with zeros
+    return `${minutes}:${secondsWithMilliseconds}`;
+  }
+  
+  // Function to fetch and display fastest lap times for the selected driver
+  function fetchFastestLapTimes() {
+    const driverSelect = document.getElementById('driverSelect');
+    const driverId = driverSelect.value;
+  
+    fetch(`/fastest_lap_times/${driverId}`)
+      .then(response => response.json())
+      .then(data => {
+        // Process the data received from the server (Flask/SQLite)
+        const lapTimes = data;
+        const lapTimesData = lapTimes.map(item => {
+          // Convert the lap time in "m:ss.SSS" format to seconds for Chart.js
+          const lapTimeParts = item.fastestLapTime.split(':');
+          const minutesInSeconds = parseInt(lapTimeParts[0]) * 60;
+          const secondsWithMilliseconds = parseFloat(lapTimeParts[1]);
+          return minutesInSeconds + secondsWithMilliseconds;
+        });
+        const raceIdsData = lapTimes.map(item => item.raceId);
+  
+        // Destroy the previous chart instance if it exists
+        if (lapTimesChart) {
+          lapTimesChart.destroy();
+        }
+  
+        // Create a new horizontal bar chart using Chart.js
+        const canvasElement = document.getElementById('lapTimesChart');
+        lapTimesChart = new Chart(canvasElement, {
+          type: 'bar',
+          data: {
+            labels: raceIdsData,
+            datasets: [
+              {
+                label: 'Fastest Lap Times',
+                data: lapTimesData,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+              },
+            ],
+          },
+          options: {
+            indexAxis: 'y',
+            scales: {
+              y: {
+                beginAtZero: true,
+                title: {
+                  display: true,
+                  text: 'Race ID',
+                },
+              },
+              x: {
+                beginAtZero: false, // Start the scale from the lowest value in the data
+                title: {
+                  display: true,
+                  text: 'Fastest Lap Time',
+                },
+                ticks: {
+                  // To set custom step size and maximum value for x-axis
+                  stepSize: 5, // Customize the step size based on your data
+                  callback: function (value) {
+                    const minutes = Math.floor(value / 60);
+                    const secondsWithMilliseconds = (value % 60).toFixed(3).padStart(6, '0');
+                    return `${minutes}:${secondsWithMilliseconds}`;
+                  },
+                },
+              },
+            },
+            responsive: true,
+            plugins: {
+              legend: {
+                display: true, 
+              },
+              title: {
+                display: true,
+                text: 'Fastest Lap Times',
+              },
+              tooltip: {
+                enabled: true,
+                callbacks: {
+                  label: function (context) {
+                    const lapTimeInSeconds = context.parsed.y; // Use parsed value directly
+                    const raceId = context.dataset.labels[context.dataIndex];
+                    return `Race ID: ${raceId}, Lap Time: ${formatTime(lapTimeInSeconds)}`;
+                  },
+                },
+              },
+            },
+          },
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+  
+  // Function to fetch and display driver standings over time for the selected driver
+  function fetchDriverStandingsOverTime() {
+    const driverSelect = document.getElementById('driverSelect');
+    const driverId = driverSelect.value;
+  
+    fetch(`/driver_standings_over_time/${driverId}`)
+      .then(response => response.json())
+      .then(data => {
+        // Process the data received from the server (Flask/SQLite)
+        const raceRounds = data.map(item => item.raceId);
+        const points = data.map(item => item.points);
+  
+        // Destroy the previous chart instance if it exists
+        if (driverStandingsChart) {
+          driverStandingsChart.destroy();
+        }
+  
+        // Create a new line chart using Chart.js
+        const canvasElement = document.getElementById('driverStandingsChart');
+        driverStandingsChart = new Chart(canvasElement, {
+          type: 'line',
+          data: {
+            labels: raceRounds,
+            datasets: [
+              {
+                label: 'Driver Standings',
+                data: points,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderWidth: 2,
+              },
+            ],
+          },
+          options: {
+            responsive: true,
+            plugins: {
+              legend: {
+                display: false,
+              },
+              title: {
+                display: true,
+                text: 'Driver Standings Over Time',
+              },
+              tooltip: {
+                enabled: true,
+                callbacks: {
+                  label: function (context) {
+                    if (context.parsed && context.parsed.y !== null) {
+                      const lapTimeInSeconds = context.parsed.y; // Use parsed value directly
+                      const raceId = context.dataset.labels[context.dataIndex];
+                      return `Race Id: ${raceId}, Points: ${lapTimeInSeconds}`;
+                    }
+                    return '';
+                  },
+                },
+              },
+            },
+            scales: {
+              y: {
+                beginAtZero: true,
+                title: {
+                  display: true,
+                  text: 'Points',
+                },
+              },
+              x: {
+                title: {
+                  display: true,
+                  text: 'Race Id',
+                },
+              },
+            },
+          },
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+  
+  // Function to fetch driver names and populate the dropdown on page load
+  function fetchDriverNames() {
+    fetch('/drivers')
+      .then(response => response.json())
+      .then(data => {
+        const driverSelect = document.getElementById('driverSelect');
+        data.forEach(driver => {
+          const option = document.createElement('option');
+          option.value = driver.driverId;
+          option.text = `${driver.firstname} ${driver.lastname}`;
+          driverSelect.appendChild(option);
+        });
+  
+        // Fetch fastest lap times for the first driver in the dropdown by default
+        fetchFastestLapTimes();
+      });
+  }
+  
+  // Fetch driver names and populate the dropdown on page load
+  fetchDriverNames();
+  
+  // Event listener for the dropdown selection change
+  document.getElementById('driverSelect').addEventListener('change', fetchDriverStandingsOverTime);
+  
